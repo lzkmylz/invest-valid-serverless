7f23adf56efe838407434900d46a80621de8e3d1
lzkmylz <lzkmylz@163.com>
2019-11-19 21:09:04 +0800

    finish update Avatar version 0.1

--- UserAttr/UpdateAvatar.js
+++ UserAttr/UpdateAvatar.js
@@ -1,30 +1,60 @@
 'use strict';
 const multipart = require('aws-lambda-multipart-parser');
+const uuidv4 = require('uuid/v4');
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'ap-southeast-1' });
+var s3 = new AWS.S3();
 
 module.exports.update = async event => {
   const parsedData = multipart.parse(event, true);
-  let filename = parsedData.file.filename;
   let data = parsedData.file.content;
-  let dataType = parsedData.file.contentType;
+  let dataType = parsedData.file.contentType.split('/');
+  let imgFormat = dataType[1];
   const s3BucketName = process.env.AvatarS3Bucket;
-  
-  console.log(filename, dataType, data, s3BucketName);
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  if(dataType[0] != 'image') {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+          message: 'Upload file is not image file!',
+      }),
+    }
+  }
+
+  let imgSaveName = uuidv4() + '.' + imgFormat;
+  let params = {
+    Bucket: s3BucketName,
+    Key: imgSaveName,
+    Body: data
+  };
+  s3.putObject(params, (err, data) => {
+    if(err) {
+      return {
+        statusCode: err.statusCode,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ 
+          error: err.name ? err.name : "Exception", 
+          message: err.message ? err.message : "Unknown error" 
+        }),
+      }
+    }
+    // success
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({
+          fileKey: imgSaveName,
+      }),
+    };
+  });
 };
