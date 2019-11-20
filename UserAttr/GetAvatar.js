88198ccd99752297ed04e7e0795e883989399bf0
lzkmylz <lzkmylz@163.com>
2019-11-20 22:33:33 +0800

    add getAvatar function

---
+++ UserAttr/GetAvatar.js
@@ -0,0 +1,39 @@
+'use strict';
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'ap-southeast-1' });
+var s3 = new AWS.S3();
+
+module.exports.get = async event => {
+  let avatarKey = event.body.key;
+  const s3BucketName = process.env.AvatarS3Bucket;
+
+  let params = {
+    Bucket: s3BucketName, 
+    Key: avatarKey, 
+   };
+  try {
+    let result = await s3.getObject(params).promise();
+      // success
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: result
+    };
+  } catch(err) {
+    console.log(err);
+    return {
+      statusCode: err.statusCode, 
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({ 
+        error: err.name ? err.name : "Exception", 
+        message: err.message ? err.message : "Unknown error" 
+      }) 
+    };
+  }
+};
