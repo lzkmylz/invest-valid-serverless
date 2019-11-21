a219e8fc162255b038cccfa28961400db8225f7c
lzkmylz <lzkmylz@163.com>
2019-11-21 23:21:56 +0800

    fuck

--- UserAttr/GetAvatar.js
+++ UserAttr/GetAvatar.js
@@ -6,18 +6,23 @@ var s3 = new AWS.S3();
 module.exports.get = async event => {
   let avatarKey = event.pathParameters.avatarName;
   const s3BucketName = process.env.AvatarS3Bucket;
+  console.log(avatarKey, s3BucketName)
   let params = {
-    Bucket: s3BucketName, 
+    Bucket: s3BucketName,
     Key: avatarKey,
   };
-  let result = await s3.getObject(params).promise();
-      // success
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: result
-  };
+  console.log(params);
+  s3.getObject(params, (err, data) => {
+    if(err) {
+      console.log(err);
+    }
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: data
+    };
+  });  
 };
