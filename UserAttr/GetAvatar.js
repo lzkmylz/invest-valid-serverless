83642f8ef3fa31c5f4c823819a88d689c59136cb
lzkmylz <lzkmylz@163.com>
2019-11-22 18:35:50 +0800

    seems fixed

--- UserAttr/GetAvatar.js
+++ UserAttr/GetAvatar.js
@@ -6,25 +6,19 @@ var s3 = new AWS.S3();
 module.exports.get = async event => {
   let avatarKey = event.pathParameters.avatarName;
   const s3BucketName = process.env.AvatarS3Bucket;
-  console.log(avatarKey, s3BucketName)
   let params = {
     Bucket: s3BucketName,
     Key: avatarKey,
   };
-  console.log(params);
-  s3.getObject(params, (err, data) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    console.log(data);
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-      body: data
-    };
-  });  
+  let result = await s3.getObject(params).promise();
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      data: result.Body.toString('base64')
+    }),
+  };
 };
