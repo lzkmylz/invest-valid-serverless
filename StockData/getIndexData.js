6f325c554825f17c2f11cb97c195ed19b84ac27b
lzkmylz <lzkmylz@163.com>
2019-12-19 13:16:04 +0800

    add get index data function

---
+++ StockData/getIndexData.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+module.exports.get = async event => {
+  let url = "http://api.waditu.com";
+  let token = process.env.TUSHARE_TOKEN
+  let data =JSON.parse(event.body);
+  let reqBody = {
+    api_name: "index_daily",
+    token: token,
+    params: data,
+    fields: ""
+  }
+  let resdata = await axios.post(url, reqBody);
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(resdata.data)
+  };
+}
